dbd59b18c86e51c25854a0a1b1c69ca60175dcc7
Amanullah Aman <amancse.aiub@gmail.com>
2017-02-14 12:32:06 +0600

    Basic Syntex Tag

---
+++ 02.basicSyntex/index.php
@@ -0,0 +1,16 @@
+<p>This is going to be ignored by PHP and displayed by the browser.</p>
+<?php echo 'While this is going to be parsed.'; ?>
+<p>This will also be ignored by PHP and displayed by the browser.</p>
+
+<?php
+    echo "PHP Basic Syntex";
+?>
+
+<?= 'Short php echo'; ?>
+
+<?php
+  echo "<br>Satement end without semicolon."
+?>
+
+<?php
+  echo "<br>Satement end without php closing tag.";
