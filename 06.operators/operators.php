70221b35e50f21fefbef58894379783af75dc129
Amanullah Aman <amancse.aiub@gmail.com>
2017-02-16 12:05:28 +0600

    PHP Operators

---
+++ 06.operators/operators.php
@@ -0,0 +1,21 @@
+<?php
+  $a = 1;
+  $b = 2;
+
+  echo $a = $b +=3 ;
+  echo "<br>";
+
+  echo "Example #2 Undefined order of evaluation<br>";
+  echo '$a = 5, now print $a + $a++: ';
+  echo $a + $a++;
+  echo "<br>";
+
+  $array[$a] = $a++;
+  print_r($array);
+
+  echo "<br>Example #3 +, - and . have the same precedence<br>";
+  $x = 4;
+  echo "x minus one equals " .$x-1 . ", or so I hope\n";
+
+  echo "x minus one equals " . ($x-1) . ", or so I hope\n";
+?>
