69d24cb5771a84844409776fedc8c4c146ddc5ba
Amanullah Aman <amancse.aiub@gmail.com>
2017-02-16 12:21:44 +0600

    Arithmetic operator

---
+++ 06.operators/arithmetic.php
@@ -0,0 +1,14 @@
+<?php
+
+echo (5 % 3)."<br>";           // prints 2
+echo (5 % -3)."<br>";          // prints 2
+echo (-5 % 3)."<br>";          // prints -2
+echo (-5 % -3)."<br>";         // prints -2
+
+echo $a = 10;
+$b = 3;
+echo " a=10 exponential b=3 [a**b]: ";
+echo $a**$b;
+echo "<br>";
+echo $b;
+?>
