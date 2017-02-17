8debc76c9314e1f32a194118a5833dbcf461467c
Amanullah <amancse.aiub@gmail.com>
2017-02-17 11:24:50 +0600

    While loop

---
+++ 07.control_structure/while.php
@@ -0,0 +1,25 @@
+<?php
+  echo "<h1>While Loop</h1>";
+
+  $i = 1;
+  while($i <= 10){
+    echo $i++;
+    echo "<br>";
+  }
+
+  echo "**********************<br>";
+  echo "Print Even Numbers 1 to 100 <br>";
+  $j = 1;
+  $sum = 0;
+  $count =1 ;
+  while($j <= 100):
+    if($j % 2 == 0):
+      echo $j."<br>";
+      $sum +=$j;
+      $count++;
+    endif;
+    $j++;
+  endwhile;
+  echo "Total Even Numbers:". ($count-1)."<br>";
+  echo "Total Sum: $sum";
+?>
