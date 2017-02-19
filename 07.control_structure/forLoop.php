a9b7af1973c68bc2ae5d19b19903d0e5842cae7a
Amanullah Aman <amancse.aiub@gmail.com>
2017-02-19 12:33:43 +0600

    For Loop

--- 07.control_structure/forLoop.php
+++ 07.control_structure/forLoop.php
@@ -24,4 +24,33 @@
 
   echo "<br>For Loop Example#4<br>";
   for ($l = 1, $m = 0; $l <= 5; $m += $l, print $l," ", $l++);
+
+  echo "<br>******************************<br>";
+  print_r(mt_rand(000000, 999999));
+
+  echo "<br>*************************<br>";
+  echo "<pre>";
+  echo "<br>This code will be slower<br>";
+  $people = array(
+      array('name' => 'Kalle', 'salt' => 856412),
+      array('name' => 'Pierre', 'salt' => 215863)
+  );
+  print_r($people);
+
+  for($i = 0; $i < count($people); ++$i) {
+      $people[$i]['salt'] = mt_rand(000000, 999999);
+  }
+  print_r($people);
+
+  echo "<br>This code will be faster then previous output<br>";
+  $people = array(
+      array('name' => 'Kalle', 'salt' => 856412),
+      array('name' => 'Pierre', 'salt' => 215863)
+  );
+  print_r($people);
+
+  for($i = 0, $size = count($people); $i < $size; ++$i) {
+      $people[$i]['salt'] = mt_rand(000000, 999999);
+  }
+  print_r($people);
 ?>
