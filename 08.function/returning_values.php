65dbc05c0fc4f37d7591e5d24bfda53104a1aaf0
Amanullah Aman <amancse.aiub@gmail.com>
2017-02-23 13:28:40 +0600

    Returning an array to get multiple values

--- 08.function/returning_values.php
+++ 08.function/returning_values.php
@@ -8,4 +8,12 @@
   echo "<br>";
   $e1 = microtime(true);
   echo "Time". ($e1 - $s1);
+
+  echo "<h2>Example #2 Returning an array to get multiple values</h2>";
+  function return_multiple(){
+    return array(0,1,2,3);
+  }
+  var_dump(return_multiple());
+  list($z,$o,$t,$th) = return_multiple();
+  echo $z;
 ?>
