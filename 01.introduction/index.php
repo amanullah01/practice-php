bc536396dc452cf87ca7ead7f5379915cd000d18
Amanullah Aman <amancse.aiub@gmail.com>
2017-02-25 12:53:09 +0600

    server info

--- 01.introduction/index.php
+++ 01.introduction/index.php
@@ -7,6 +7,9 @@
 
         <?php
             echo "Hi, I'm a PHP script!";
+            echo "<br>";
+            echo "Main root: ".$_SERVER['DOCUMENT_ROOT'];
+            echo "<br>Server name: ".$_SERVER['SERVER_NAME'];
         ?>
 
     </body>
