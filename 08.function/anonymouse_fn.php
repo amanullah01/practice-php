3adf7c7ab852abe0e5936a7d58723d59239241dc
Amanullah Aman <amancse.aiub@gmail.com>
2017-02-26 11:03:27 +0600

    function assignment in variables

--- 08.function/anonymouse_fn.php
+++ 08.function/anonymouse_fn.php
@@ -3,4 +3,29 @@
   echo preg_replace_callback('~-([a-z])~', function ($match) {
       return strtoupper($match[1]);
   }, 'hello-world-people');
-?>
+
+  echo "<h2>Example #2 Anonymous function variable assignment example</h2>";
+
+  $great = function($name){
+    echo "Hello $name <br>";
+  };
+  $great('Aman');
+  $great('PHP World');
+
+  $ar = array(
+    1 => 'Hello',
+    ald =>'World',
+    false => 'What'
+  );
+  echo "<pre>";
+  print_r($ar);
+
+  echo "<h2>Example #3 Inheriting variables from the parent scope</h2>";
+
+  $message = 'Hello';
+  $example = function (){
+    var_dump($message);
+  };
+  $example();
+
+  //
