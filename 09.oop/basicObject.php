3ba44b84057752e6449d04da7477ca625ab7ef72
Amanullah Aman <amancse.aiub@gmail.com>
2017-02-27 11:43:59 +0600

    pseudo variable

--- 09.oop/basicObject.php
+++ 09.oop/basicObject.php
@@ -10,4 +10,30 @@
 
   $obj1 = new SimpleClass ;
   $obj1->simpleMethod();
+
+  echo '<h2>Example #2 Some examples of the $this pseudo-variable</h2>';
+  echo '<h4>$this is a reference to the calling object</h4>';
+
+  class A{
+    function foo(){
+      if(isset($this)){
+        echo "This is defined (". get_class($this).")<br>";
+      }else{
+        echo "Not defined<br>";
+      }
+    }
+  }
+  class B{
+    function bar(){
+      A::foo();
+    }
+  }
+
+  $obj2 = new A ;
+  $obj2->foo();
+  A::foo();
+
+  $obj3 = new B;
+  $obj3->bar();
+  B::bar();
 ?>
