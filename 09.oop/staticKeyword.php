6d795e91440e5bc609b1f7d831cecfd281c37f31
Amanullah <amancse.aiub@gmail.com>
2017-03-03 12:19:03 +0600

    Static method example

---
+++ 09.oop/staticKeyword.php
@@ -0,0 +1,10 @@
+<?php
+  class MyClass{
+    public static function myState(){
+      echo "This is static function<br>";
+    }
+  }
+  echo "<h2>Example #1 Static method example</h2>";
+
+  MyClass::myState();
+?>
