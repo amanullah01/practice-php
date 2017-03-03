e612ef4963a571ee004d53456cddc46b55c347ca
Amanullah <amancse.aiub@gmail.com>
2017-03-03 11:50:02 +0600

    Inside inside the class defination

--- 09.oop/scopeResoulation.php
+++ 09.oop/scopeResoulation.php
@@ -7,4 +7,23 @@
   $obj = new ScopeResoulation ;
   var_dump($obj);
   echo ScopeResoulation::CONST_VALUE;
+
+  echo "<h2>Example #2 :: from inside the class definition</h2>";
+
+  class OtherClass extends ScopeResoulation{
+    public static $my_static = "Static var";
+
+    public static function foo(){
+      echo parent::CONST_VALUE;
+      echo "<br>";
+      echo self::$my_static;
+      echo "<br>";
+      echo self::CONST_VALUE;
+    }
+  }
+
+  $obj2 = new OtherClass ;
+  $obj2->foo();
+  echo "<br>****************************</br>";
+  OtherClass::foo();
 ?>
