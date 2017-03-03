90f27fb5a22d4c41c43ea99de4a7595487f75131
Amanullah <amancse.aiub@gmail.com>
2017-03-03 11:55:59 +0600

    Calling parents method

--- 09.oop/scopeResoulation.php
+++ 09.oop/scopeResoulation.php
@@ -1,11 +1,16 @@
 <?php
   class ScopeResoulation{
     const CONST_VALUE = "Hello Scope";
+
+    protected function myFunc(){
+      echo "Hello ... This is protected myFunc method in ".__CLASS__."<br>";
+    }
   }
   echo "<pre>";
   echo "<h2>Example #1 :: from outside the class definition</h2>";
   $obj = new ScopeResoulation ;
   var_dump($obj);
+  //echo $obj->CONST_VALUE; // It will not work here.
   echo ScopeResoulation::CONST_VALUE;
 
   echo "<h2>Example #2 :: from inside the class definition</h2>";
@@ -26,4 +31,16 @@
   $obj2->foo();
   echo "<br>****************************</br>";
   OtherClass::foo();
+
+  echo "<h2>Example #3 Calling a parent's method</h2>";
+
+  class NewMyClass extends ScopeResoulation{
+    function myFunc(){
+      parent::myFunc();
+      echo "This is public my func in ".__CLASS__." Class";
+    }
+  }
+
+  $obj3 = new NewMyClass ;
+  $obj3->myFunc();
 ?>
