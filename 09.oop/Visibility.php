6c234428285ce933f019128b757611d5ccfce110
Amanullah <amancse.aiub@gmail.com>
2017-03-03 09:55:59 +0600

    Property visibility

--- 09.oop/Visibility.php
+++ 09.oop/Visibility.php
@@ -18,7 +18,7 @@ class Visibility
         echo $this->private;
     }
 }
-
+echo "<pre>";
 $obj = new Visibility();
 echo $obj->public;
 echo "<br>";
@@ -26,3 +26,25 @@ echo "<br>";
 //echo $obj->private; //fatal error
 
 $obj->printHello(); //this function can access all private and protected property.
+
+echo "<br>************************* MyClass ***********************<br>";
+class MyClass extends Visibility{
+  public $public = "Public Property 2";
+  protected $protected = "Protected Property 2";
+
+  public function printHello(){
+    echo $this->public.'<br>';
+    echo $this->protected.'<br>';
+    echo $this->private.'<br>'; //this is undefined property.
+  }
+}
+
+$obj2 = new MyClass();
+var_dump($obj2);
+echo $obj->public;
+echo "<br>";
+//echo $obj2->protected; // it shows fatal error....
+echo "<br>";
+echo $obj2->private; //this is undefined property.
+echo "<br>";
+echo $obj2->printHello();
