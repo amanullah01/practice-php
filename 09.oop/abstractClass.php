2ee5b1a45a6e6f55cc66781e2a23580373583d0a
Amanullah Aman <amancse.aiub@gmail.com>
2017-03-20 13:06:28 +0600

    Abstract class

--- 09.oop/abstractClass.php
+++ 09.oop/abstractClass.php
@@ -19,8 +19,23 @@
     }
   }
 
+  class ChildClass2 extends AbstractClass{
+    protected function getValue(){
+      $a = [1,2,3,4,5,6,7];
+      return $a;
+    }
+
+    public function prefixValue($prefix){
+      echo $prefix;
+    }
+  }
+
   $obj1 = new ChildClass1 ;
   $obj1->printValue();
   $obj1->prefixValue('Aman');
+  echo "<br>******************<br>";
+  $obj2 = new ChildClass2 ;
+  $obj2->printValue();
+  $obj2->prefixValue('Amanullah Aman');
 
 ?>
