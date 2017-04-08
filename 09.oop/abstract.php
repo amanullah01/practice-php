d8e2c23f3c5aa85e0cf612caa0d7fa3d9806a9a4
Amanullah Aman <amancse.aiub@gmail.com>
2017-04-08 11:40:52 +0600

    abstract class

--- 09.oop/abstract.php
+++ 09.oop/abstract.php
@@ -6,8 +6,10 @@
   class MyAbstract
   {
     var $val;
+    var $tax;
     abstract protected function getValue();
     abstract protected function getPrefix($pre);
+    abstract protected function getTax($tax);
 
     public function getPrint(){
       return $this->getValue();
@@ -20,17 +22,24 @@
   class ClassName1 extends MyAbstract
   {
     protected function getValue(){
-      return 'This is get value from class name 1';
+      return 'Test get value';
     }
 
     public function getPrefix($pre){
       return '<br>'.$pre.' this is prefix';
     }
+
+    public function getTax($tax){
+      return '<br>BDT '.((float)$tax*.15);
+    }
   }
 
   $c1 = new ClassName1();
+
   echo $c1->getPrint();
   echo $c1->getPrefix('Aman');
+  echo $c1->getTax(200);
+
 
 require "../footer.php";
 ?>
