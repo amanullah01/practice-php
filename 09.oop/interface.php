f125429b3030bc59a607ab26383fe803e9aaa0d4
Amanullah <amancse.aiub@gmail.com>
2017-04-09 22:38:23 +0600

    interface implements

--- 09.oop/interface.php
+++ 09.oop/interface.php
@@ -9,18 +9,30 @@
   class MiniCar implements Car{
     private $carName;
     private $modelName;
+    private $car=[];
     public function setInfo($carName,$modelName){
-      $this->carName = $carName;
-      $this->modelName = $modelName;
+      // $this->carName = $carName;
+      // $this->modelName = $modelName;
+      $this->car[$carName] = $modelName;
     }
 
     public function getInfo(){
-      return 'Car Name: '.$this->carName.'<br>Model Name: '.$this->modelName;
+      //return json_encode($this->car);
+      $str = '';
+      foreach ($this->car as $key => $value) {
+        $str.='Car Name: '.$key.', Model: '.$value.'<br>';
+      }
+      return $str;
     }
   }
 
   $car = new MiniCar;
   $car->setInfo('Toyota', 'Personal');
+  $car->setInfo('Corolla', 'X');
+  $car->setInfo('Audi', 'A8');
+  //echo "<pre>";
+  //print_r($car->getInfo());
   echo $car->getInfo();
+  //echo $car->getInfo();
   require "../footer.php";
 ?>
